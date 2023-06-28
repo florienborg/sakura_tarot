29f6efa4abedf688253ad82b0579d1cd0892a7f7
florienborg <flornbg014@gmail.com>
2023-06-28 15:50:56 +0200

    Mockup pag 3, titulo, api card y api text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import WelcomeText from './components/welcometext/welcometext';
 import ApiText from './components/apitext/apitext';  
+import TitleAndPic from './components/MockupPag3-TitleAndPic/titleandpic';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeText/>
+      <TitleAndPic/>
+      <ApiText/>
     </div>
   );
 }
---
+++ src/components/MockupPag3-TitleAndPic/titleandpic.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './titleandpic.css';
+
+const TitleAndPic = () =>{
+    return (
+        <div className="title-and-pic">
+            <div className="title">
+                <h1>Past</h1>
+            </div>
+            <div className="api-card">
+                <img className="api-card" src="https://i.ibb.co/hmyxC61/Canci-n.jpg" alt="ClowCard" />
+            </div>
+        </div>
+    )
+};
+
+export default TitleAndPic;
