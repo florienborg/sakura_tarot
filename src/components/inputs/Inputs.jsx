f5c2f8d5f324847fbbd4064852c91afc47e70afe
Nelora Mood <lorenamoralesodena@gmail.com>
2023-06-28 11:54:13 +0200

    inputs corrections

--- src/components/inputs/Inputs.jsx
+++ src/components/inputs/Inputs.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import './inputs.css';
 
 function Form() {
-
   const [formData, setFormData] = useState({
     name: '',
-    bithdate: '',
+    birthdate: '',
     mood: '',
   });
 
@@ -14,9 +13,8 @@ function Form() {
     console.log(formData);
     setFormData({
       name: '',
-      bithdate: '',
+      birthdate: '',
       mood: '',
-      registered: '',
     });
     const registeredText = document.getElementById('registered');
     registeredText.removeAttribute('hidden');
@@ -32,45 +30,51 @@ function Form() {
   };
 
   return (
-    
-     
-      <form className="container-form" onSubmit={handleSubmit}>
-        <label htmlFor="name" className="text-form">
-          Name:
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="input-form"
-          placeholder='Enter your name'
-          required
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="birthdate" className="text-form">
-         Birthdate:
-        </label>
-        <input
-          type="text"
-          id="birthdate"
-          name="birthdate"
-          className="input-form"
-          placeholder='Enter your birthdate'
-          value={formData.bithdate}
-    
-          onChange={handleChange}
-        />
-        <label htmlFor="mood" className="text-form">
-          Mood
-        </label>
-        <input
-          type="mood"
-          id="mood"
-          name="mood"
-          className="input-form"
-          placeholder='Write what you feel before reading. Max 100 chars.' 
-          value={formData.mood}
-          onChange={handleChange}
-        />
-        }
+    <form className="container-input" onSubmit={handleSubmit}>
+      <label htmlFor="name" className="text-input">
+        Name:
+      </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        className="input-form"
+        placeholder="Enter your name"
+        required
+        value={formData.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="birthdate" className="text-input">
+        Birthdate:
+      </label>
+      <input
+        type="text"
+        id="birthdate"
+        name="birthdate"
+        className="input-form"
+        placeholder="Enter your birthdate"
+        value={formData.birthdate}
+        onChange={handleChange}
+      />
+      <label htmlFor="mood" className="text-input">
+        Mood:
+      </label>
+      <input
+        type="text"
+        id="mood"
+        name="mood"
+        className="input-form"
+        placeholder="Write what you feel before reading. Max 100 chars."
+        value={formData.mood}
+        maxLength={100} //  limit 100 characters
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+      <p id="registered" hidden>
+        Registered successfully!
+      </p>
+    </form>
+  );
+}
+
+export default Form;
