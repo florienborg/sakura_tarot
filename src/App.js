77f1043cb0ab19603e0d3a0c40f7f9adf18d8524
leandrita <lea.bujh@gmail.com>
2023-06-29 16:31:17 +0200

    add redButton to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
 import WelcomeText from './components/welcometext/welcometext';
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import RedButton from './components/RedButton/RedButton'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <WelcomeText />
+      <RedButton />
     </div>
   )
 
