f70b9d7fc60615e1a146b62391bb1626103f243d
Laura <garch.laura@gmail.com>
2023-06-29 20:03:52 +0200

    some changes in organization

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import React from 'react';
-import WelcomeText from './components/welcometext/welcometext';
-import Header from './components/header/Header';
-import RedButton from './components/RedButton/RedButton';
-import Form from './components/form/Form';
+import IndexPage from './pages/index';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <WelcomeText/>
-      <Form/>
-      <RedButton/>
+      <IndexPage />
     </div>
   )
 
---
+++ src/pages/index.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import React from 'react';
+import WelcomeText from '../components/welcometext/welcometext';
+import Header from '../components/header/Header';
+import RedButton from '../components/RedButton/RedButton';
+import Form from '../components/form/Form';
+
+function indexPage() {
+  return (
+    <div className="App">
+      <Header />
+      <WelcomeText/>
+      <Form/>
+      <RedButton />
+    </div>
+  )
+
+}
+
+export default indexPage;
