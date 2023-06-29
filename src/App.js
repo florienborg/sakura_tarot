6bc85336007e44e5f9af2319d192ca3bf5e509c0
Nelora Mood <lorenamoralesodena@gmail.com>
2023-06-29 16:15:06 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import WelcomeText from './components/welcometext/welcometext';
 import Header from './components/header/Header'  
+import Form from './components/form/Form';
 
 function App() {
   return (
     <div className="App">
             <Header/>
       <WelcomeText/>
+      <Form />
     </div>
   )
    
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './inputs.css';
+import './Form.css';
 
 function Form() {
   const [formData, setFormData] = useState({
