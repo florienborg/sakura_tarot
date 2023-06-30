bca20d5c197c3a0474bee306b0e8e2343d37ffe5
florienborg <flornbg014@gmail.com>
2023-06-30 15:10:24 +0200

    borrar carpeta welcometext y paginas game e index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import React from 'react';
-// import WelcomeText from './components/welcometext/welcometext';
-// import ApiText from './components/apitext/apitext';  
-// import TitleAndPic from './components/MockupPag3-TitleAndPic/titleandpic';
 import SavedReadings from './components/mockup_saved_readings/savedreadings';
 
 function App() {
   return (
     <div className="App">
-      <SavedReadings/>
     </div>
   );
 }
