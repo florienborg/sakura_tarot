1ce4f3f48e300714003439e25090c8cb8a128ef2
leandrita <lea.bujh@gmail.com>
2023-07-05 14:58:19 +0200

    styles page game and media queries

--- src/applications/routes.jsx
+++ src/applications/routes.jsx
@@ -1,6 +1,5 @@
 import Home from "../pages/home/home.jsx";
 import { createBrowserRouter } from "react-router-dom";
-import Page3 from "../pages/cards/pag3mockup.jsx";
 import Game from "../pages/game/game.jsx";
 import SavedReadings from "../pages/saved/savedreadings.jsx";
 
@@ -15,12 +14,6 @@ const routes = [
     path: "/game",
     element: <Game />,
   },
-
-  {
-    path: "/cards",
-    element: <Page3 />,
-  },
-
 
   {
     path: "/saved",
