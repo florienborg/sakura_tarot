5b8e8a013fd6a4a4de76b656f6ea2bd2c5b2fdd4
florienborg <flornbg014@gmail.com>
2023-07-05 14:49:11 +0200

    historial eliminado en db.json y eliminé en routes.jsx el de la pagina3 (mockup) que ya no existe

--- src/applications/routes.jsx
+++ src/applications/routes.jsx
@@ -1,6 +1,5 @@
 import Home from "../pages/home/home.jsx";
 import { createBrowserRouter } from "react-router-dom";
-import Page3 from "../pages/cards/pag3mockup.jsx";
 import Game from "../pages/game/game.jsx";
 import SavedReadings from "../pages/saved/savedreadings.jsx";
 
@@ -16,10 +15,7 @@ const routes = [
     element: <Game />,
   },
 
-  {
-    path: "/cards",
-    element: <Page3 />,
-  },
+
 
 
   {
