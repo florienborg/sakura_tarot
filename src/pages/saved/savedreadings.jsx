d1351c509f0f6553c463d5d607ee383cf4b879d6
florienborg <flornbg014@gmail.com>
2023-07-06 16:31:58 +0200

    mostrar el mood en pagina savedreadings

--- src/pages/saved/savedreadings.jsx
+++ src/pages/saved/savedreadings.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import ApiText from "../../components/apitext/apitext";
 import './savedreadings.css';
 import { useLocation } from "react-router-dom";
+import db from "../../db.json"
 
 
 const SavedReadings = () =>{
 const { state } = useLocation();
 console.log(state);
+console.log(db);
+
     return (
+        <div>
+        {db.formdata.map(form => (
+          <p> {form.mood} </p>
+        ) )}
+        
     <div className="savings-container">
         <div className="title">
             Saved Readings
@@ -20,7 +28,7 @@ console.log(state);
         </div>
             <ApiText customClass='custom-apitext'/>
     </div>
-    )
-};
+    </div>
+)};
 
 export default SavedReadings;
